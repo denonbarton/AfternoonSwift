d45099ed4623a2b95b29829dda12d524c59cba77
denonbarton <dbar0540@740s-pl-126.local>
2015-12-03 13:37:24 -0700

    1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explicitt declaratino of a String
+let AFTERNOON_PLAYGROUND :String = "Learning Swift"
+var myWords = AFTERNOON_PLAYGROUND //Implicitly a String
+myWords = "newStuff"
+print(myWords)
+print(AFTERNOON_PLAYGROUND)
+myWords = "other words"
+
+var otherVariable = 7
+otherVariable = Int (3.9999) //casting a Double as an Int
